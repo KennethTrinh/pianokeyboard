6d8262a4a4687346511c576f77d0845e14a78951
KennethTrinh <klt45@cornell.edu>
2026-04-20 17:50:02 -0400

    improve playlist style, add new songs

--- src/Playlist.js
+++ src/Playlist.js
@@ -31,34 +31,6 @@ const Playlist = (props) => {
     setPlaylist([...playlist, newSong]);
   };
 
-  const addSong = (song) => {
-    setPlaylist([...playlist, song]);
-  };
-
-  const next = () => {
-    let nextIndex =
-      playlist.findIndex(
-        (song) => song.name === currentSong && song.url === currentSongURL,
-      ) + 1;
-    if (nextIndex === playlist.length) {
-      nextIndex = 0;
-    }
-    setCurrentSong(playlist[nextIndex].name);
-    setCurrentSongURL(playlist[nextIndex].url);
-  };
-
-  const prev = () => {
-    let prevIndex =
-      playlist.findIndex(
-        (song) => song.name === currentSong && song.url === currentSongURL,
-      ) - 1;
-    if (prevIndex < 0) {
-      prevIndex = playlist.length - 1;
-    }
-    setCurrentSong(playlist[prevIndex].name);
-    setCurrentSongURL(playlist[prevIndex].url);
-  };
-
   const handleClick = (song) => {
     setCurrentSong(song.name);
     setCurrentSongURL(song.url);
@@ -66,35 +38,23 @@ const Playlist = (props) => {
 
   return (
     <div style={{ textAlign: "center" }}>
-      <h1
-        style={{
-          fontSize: "2em",
-          fontWeight: "bold",
-          marginTop: "20px",
-          color: "#ff69b4",
-        }}
-      >
-        My Hopeless Romantic Playlist
-      </h1>
-      <ul style={{ listStyleType: "none" }}>
-        {playlist.map((song) => (
-          <li
-            key={song.name}
-            onClick={() => handleClick(song)}
-            className="song-item"
-          >
-            {song.name}
-          </li>
-        ))}
-      </ul>
-      <input type="file" accept=".mid" onChange={handleFileChange} />
-      <button className="btn" onClick={prev}>
-        Previous
-      </button>
-      <button className="btn" onClick={next}>
-        Next
-      </button>
-      <p>Current Song: {currentSong}</p>
+      <div className="playlist-container">
+        <p className="playlist-title">My Hopeless Romantic Playlist</p>
+        <p className="playlist-hint">Click a song to play</p>
+        <ul className="playlist-ul">
+          {playlist.map((song) => (
+            <li
+              key={song.name}
+              onClick={() => handleClick(song)}
+              className={`song-item${song.name === currentSong ? " active" : ""}`}
+            >
+              <span>{song.name === currentSong ? "🎵" : "🎹"}</span>
+              {song.name}
+            </li>
+          ))}
+        </ul>
+        <input type="file" accept=".mid" onChange={handleFileChange} />
+      </div>
     </div>
   );
 };
